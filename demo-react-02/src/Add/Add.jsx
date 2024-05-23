694a4f2ec71f76b964d19b36b2f59817c374c92d
loudovsky <luileptitecossais@gmail.com>
2024-05-23 16:15:02 +0200

    push before closing today

--- demo-react-02/src/Add/Add.jsx
+++ demo-react-02/src/Add/Add.jsx
@@ -1,34 +1,40 @@
 import { useState } from "react";
 
-const Add = ({ table = 100}) => {
+const INITIAL_COUNT = 0;
+
+const Add = ({ step = 100}) => {
 
     //! State pour stocker la valeur actuelle de la table de multiplication
-    const [result, setResult] = useState(0);
+    const [result, setResult] = useState(INITIAL_COUNT);
+    const condition = false;
 
     //! Fonction qui va se déclencher lorsque l'utilisateur clique sur "x3"
-    const handleMultiplication = () => {
-        console.log(`On additionne ${table} !!!`);
+    const handleAddition = () => {
+        console.log(`On additionne ${step} !!!`);
 
         console.log('Avant', result);
-        setResult(result => result + table);
+        setResult(result => result + step);
         console.log('Après', result);
     }
 
     //! Fonction pour remettre la valeur à 0
     const handleReset = () => {
         console.log(`Reset !!!`);
-        setResult(result => 0);
+        setResult(INITIAL_COUNT);
     }
 
     //! Rendu du composant
     return (
-        <div>
-            <h2>Addition par {table}</h2>
-            <h3>La valeur : {result}</h3>
-            <button onClick={handleMultiplication}> +{table} </button>
-            <button onClick={handleReset}> Reset </button>
-        </div>
-    )
-}
+        <>
+            <p>Compteur : {result}</p>
+            <button onClick={handleAddition}> +{step} </button>
+            {result !== INITIAL_COUNT && (
+                <button onClick={handleReset}> Reset </button>
+            )
+            }
+            
+        </>
+    );
+};
 
 export default Add;
--- demo-react-02/src/App.jsx
+++ demo-react-02/src/App.jsx
@@ -7,7 +7,10 @@ function App() {
   return (
     <>
       <h1>Demo 02</h1>
-      {/*<Multi />*/}
+      <h2>Exo - 2</h2>
+      {/*<h2>Exo - 1</h2>
+      <Multi />*/}
+      
       <Add />
     </>
   )
