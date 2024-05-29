e452834a1720d27f527f02742ec2a45f31dcbcbb
loudovsky <luileptitecossais@gmail.com>
2024-05-29 15:55:57 +0200

    +tasklist & filtre des résultats

--- demo-react-04/demo-react-04/src/components/TaskList/TaskList.jsx
+++ demo-react-04/demo-react-04/src/components/TaskList/TaskList.jsx
@@ -1,9 +1,10 @@
 import clsx  from 'clsx';
 import style from './TaskList.module.scss';
+import { useId, useState } from 'react';
 
 
 //Composant qui symbolise un élément de la liste
-const TaskItem = ({id, name, desc, priority, isDone}) => {
+const TaskItem = ({id, name, desc, priority, isDone, onDelete, onFinish}) => {
 
     return (
         <article className={clsx(style.task, isDone && style.finish)}>
@@ -12,8 +13,8 @@ const TaskItem = ({id, name, desc, priority, isDone}) => {
                     <p>{desc}</p>
                 </div>
                 <div className={style.action}>
-                    <button disabled={isDone}>Terminer</button>
-                    <button>Supprimer</button>
+                    <button onClick={() => onFinish(id)} disabled={isDone}>Terminer</button>
+                    <button onClick={() => onDelete(id)}>Supprimer</button>
                 </div>
         </article>
     );
@@ -21,15 +22,78 @@ const TaskItem = ({id, name, desc, priority, isDone}) => {
 
 //Composant qui représente la liste
 const TaskList = ({
-    tasks = [] //! Propriétés (props) de "Tasks" pour obtenir la liste des tâches du parent (le nom peut être différent)
+    tasks = [], //! Propriétés (props) de "Tasks" pour obtenir la liste des tâches du parent (le nom peut être différent)
+    onTaskDelete = () => {}, //! Props "onTaskDelete" pour informer le parent qu'on souhaite supprimer la tâche (par défaut : NOOP)
+    onTaskFinish = () => {}, //! props
 }) => {
 
+    const checkboxId = useId(); 
+    const [taskFilter, setTaskFilter] = useState({
+        high: false,
+        normal: false,
+        low: false,
+    });
+
+    const handleFilterAll = () => {        
+
+        // Abre de décision:
+
+        if(taskFilter.high && taskFilter.normal && taskFilter.low) {
+            // Si tout est coché => on décoche tout
+            setTaskFilter({high: false, normal: false, low: false});
+        }
+        
+        else {
+            // Sinon, tout cocher
+            setTaskFilter({high: true, normal: true, low: true});
+
+        }
+        
+    }                       
+
+    const handleFilter = (e) => {
+
+        const checkboxName = e.target.name; // contient uniquement high / normal / low (attribut "name" des balises)
+        
+        setTaskFilter(taskF => {
+            // Je crée un nouvel objet 
+            return {
+                ...taskF, //copie de toutes les valeurs actuelles présentes dans le state
+                [checkboxName]: !taskF[checkboxName] // Le ! permet d'inverser la valeur (si false => true, si true => false)
+                //Je modifie la valeur ciblée par le "name" de la checkbox 
+                //NB : grâce à l'opérateur d'accès [] pour manipuler les propriétés du state (Objet)
+            }
+        })        
+
+    }
+
+    const tasksWithFilter = tasks.filter(elem => (taskFilter.high && elem.priority === 'high' )
+                                        || (taskFilter.normal && elem.priority === 'normal' )
+                                        || (taskFilter.low && elem.priority === 'low' ));
     return (
+        <>
+        <div>
+           <input type="checkbox" id={checkboxId + '-all'}
+                                onChange={handleFilterAll} checked={taskFilter.high && taskFilter.normal && taskFilter.low}  />
+           <label htmlFor={checkboxId + '-all'}>Tous</label>
+           <input type="checkbox" id={checkboxId + '-high'} 
+                               name= 'high' onChange={handleFilter} checked={taskFilter.high} /> 
+           <label htmlFor={checkboxId + '-high'}>Urgent</label>
+           <input type="checkbox" id={checkboxId + '-normal'} 
+                                name = 'normal' onChange={handleFilter}  checked={taskFilter.normal} /> 
+           <label htmlFor={checkboxId + '-normal'}>Normal</label>
+           <input type="checkbox" id={checkboxId + '-low'}
+                               name = 'low' onChange={handleFilter}  checked={taskFilter.low} /> 
+           <label htmlFor={checkboxId + '-low'}>Basse</label> 
+        </div>
         <section className={style["task-list"]}>
-            {tasks.map(
-                (task, index) => <TaskItem {...task} key={task.id} />
+            {tasksWithFilter.map(
+                (task) => <TaskItem {...task} key={task.id} 
+                                onDelete={onTaskDelete}
+                                onFinish={onTaskFinish}/>
             )}
         </section>
+        </>
     )
 }
 
--- demo-react-04/demo-react-04/src/containers/TodoList/TodoList.jsx
+++ demo-react-04/demo-react-04/src/containers/TodoList/TodoList.jsx
@@ -34,6 +34,49 @@ const Todolist = () => {
         setTasks(prevTasks => [...prevTasks, task])
     };
 
+    // Gestion de la suppression d'un élément via un event de la "Tasklist"
+    const handleDeleteTask = (taskId) => {
+        
+        //? V1 - Utilisation du 'splice' (version un peu longue à écrire, mais qui fonctionne bien)
+       /*setTasks(tasks => {
+            //Copie des données
+            const copy = structuredClone(tasks);
+            
+            //Supprimer l'élément dans la copie
+            const indexTarget = copy.findIndex( elem => elem.id === taskId);
+            copy.splice(indexTarget, 1)
+
+            //Renvoyer la copie modifiée
+            return copy;
+        });*/
+
+        //? V2  - Utilisation de 'toSpliced' (pas de copie néc. car toSpliced en fait déjà une)
+        /*setTasks(tasks => tasks.toSpliced(tasks.findIndex(elem.id === taskId), 1))*/
+
+        //? V3 - Utilisation du 'filter', on met un filtre qui récupère tous les éléments sauf l'élément qu'on veut supprimer (idem que sur toSplice, pas besoin de faire une copie)
+        setTasks(tasks => tasks.filter(elem => elem.id !== taskId ));
+    };
+
+    const handleFinishTask = (taskId) => {
+        
+        //? V1 Récupération d'un élément et validation de celui-ci
+        /*
+        setTasks( tasks => {
+            const copy = structuredClone(tasks);
+
+            const targetTask = copy.find(elem => elem.id === taskId)
+
+            targetTask.isDone = true
+
+            return copy
+        });
+        */
+
+       //? V2 Utilisation de la fonction "map" qui permet de transformer une collection (/ex en javascript) en une autre collection (/ex en JSX)
+       setTasks(tasks=>tasks.map(elem => (elem.id !== taskId) ? elem : {...elem, isDone : true}));  
+    }
+
+
     return (
         <>
             <h2>Ajouter une tâche</h2>
@@ -42,7 +85,7 @@ const Todolist = () => {
             {/* Composant : list de tâches*/}
 
             <h2>Liste des tâches</h2>
-            <TaskList tasks={tasks}/>
+            <TaskList tasks={tasks} onTaskDelete={handleDeleteTask} onTaskFinish={handleFinishTask}/>
         </>
     );
 }
