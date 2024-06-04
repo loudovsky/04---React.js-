d7894932ea4e47d84192cbe93d3d7523d2045f31
loudovsky <luileptitecossais@gmail.com>
2024-06-04 15:29:18 +0200

    demo next.js pokemon

---
+++ demo-react-07/demo-next-js/src/container/Header/header.jsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+
+
+const Header = () => {
+
+    return (
+        <header>
+            <nav>
+                <ul>
+                    <li>
+                        <Link href="/">Accueil</Link>
+                    </li>
+                    <li>
+                        <Link href="/region">Régions</Link>
+                    </li>
+                    <li>
+                        <Link href="/pokemon">Pokémon</Link>
+                    </li>
+                    <li>
+                        <Link href="/about">À propos</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
